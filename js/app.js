aee60657ac25c718001cc070fd2284a71930bf78
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-08-11 08:18:52 +0200

    Create a default Launchpad program

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 (function(launchpad){
-    console.log('Hello, World!');
+    launchpad.connect({name: 'Launchpad Mini MIDI 1'}).then(function(pad){
+        pad.clear();
+        pad.on('press', function(button){
+            button.turn('red');
+        });
+    }).catch(function(error){
+        console.log(error);
+    });
 })(launchpad);
