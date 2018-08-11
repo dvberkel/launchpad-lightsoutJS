12a31f9a69a055097f820ed3010f8893ae614f27
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-08-11 08:40:29 +0200

    Create a pain program

--- js/app.js
+++ js/app.js
@@ -1,10 +1,38 @@
 (function(launchpad){
-    launchpad.connect({name: 'Launchpad Mini MIDI 1'}).then(function(pad){
-        pad.clear();
-        pad.on('press', function(button){
-            button.turn('red');
+    var colors = ['off', 'red', 'green', 'orange'];
+
+    launchpad.connect({name: 'Launchpad Mini MIDI 1'})
+        .then(function(pad){
+            var color = 'red';
+            initialize(pad, color);
+            pad.on('press', function(button){
+                if (button.isControl()){
+                    color = colors[button.id] || colors[0];
+                }
+                if (button.isControl() && button.id === 7) {
+                    color = 'red';
+                    initialize(pad, color);
+                }
+                pad.controlButton(7).turn(color);
+            });
+            pad.on('press', function(button){
+                if (!button.isControl()){
+                    button.turn(color);
+                }
+            });
+            pad.on('press', function(button){
+                console.log('%s, %s', button.channel, button.note);
+            });
+            window.pad = pad;
+        }).catch(function(error){
+            console.log(error);
         });
-    }).catch(function(error){
-        console.log(error);
-    });
+
+    function initialize(pad, color) {
+        pad.clear();
+        colors.forEach(function(color, index){
+            pad.controlButton(index).turn(color);
+        });
+        pad.controlButton(7).turn(color);
+    }
 })(launchpad);
